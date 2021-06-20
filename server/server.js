b155be6e43fee5b2cbe7be86226c7ede9cfa16d2
Min Kyung Kang <mnk918p@gmail.com>
2021-06-20 17:13:27 +0900

    set heroku

--- server/server.js
+++ server/server.js
@@ -3,7 +3,16 @@ const cors = require('cors');
 const app = express();
 const api = require('./routes/index');
 
+const PORT = process.env.PORT || 4000;
+
 app.use(cors());
 app.use('/api', api);
 
-app.listen(4000, () => console.log('노드 러닝!'));
+const buildPath = path.join(__dirname, '..', 'build');
+app.use(express.static(buildPath));
+
+app.listen(PORT, () =>
+  console.log(`
+  노드 러닝 on port: ${PORT}
+`)
+);
